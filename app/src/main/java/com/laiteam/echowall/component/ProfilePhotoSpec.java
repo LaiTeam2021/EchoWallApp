057b690ae1940e830090c40f8f27520e476aa735
Na Li <nali@nas-imac.myfiosgateway.com>
2021-03-28 23:09:40 -0400

    updated profile

--- app/src/main/java/com/laiteam/echowall/component/ProfilePhotoSpec.java
+++ app/src/main/java/com/laiteam/echowall/component/ProfilePhotoSpec.java
@@ -1,12 +1,16 @@
 package com.laiteam.echowall.component;
 
+import android.graphics.drawable.Drawable;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.generic.RoundingParams;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
 import com.facebook.litho.annotations.Prop;
+import com.facebook.litho.annotations.ResType;
 import com.facebook.litho.fresco.FrescoImage;
 
 
@@ -14,12 +18,14 @@ import com.facebook.litho.fresco.FrescoImage;
 public class ProfilePhotoSpec {
     @OnCreateLayout
     static Component onCreateLayout(ComponentContext c, @Prop String imageUrl,
-                                    @Prop int w, @Prop int h, @Prop int failureRes) {
+                                    @Prop(resType = ResType.DIMEN_SIZE) int w,
+                                    @Prop(resType = ResType.DIMEN_SIZE) int h,
+                                    @Prop(optional = true, resType = ResType.DRAWABLE) Drawable failureRes) {
 
         return FrescoImage.create(c)
-                .controller(getImageComponent(imageUrl))
-                .failureImageRes(failureRes)
-                .widthDip(w).heightDip(h)
+                .controller(getImageComponent(imageUrl)).roundingParams(RoundingParams.fromCornersRadii(w/2,w/2,w/2,w/2))
+                .failureImage(failureRes)
+                .widthPx(w).heightPx(h)
                 .build();
     }
 
